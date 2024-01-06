c69976dfa1b3baa2f2a9c619773fea4de0680643
MOON <moonjs1018@naver.com>
2024-01-06 10:25:18 +0900

    Static 변수 예제1

--- src/access/ex/ShoppingCart.java
+++ src/access/ex/ShoppingCart.java
@@ -6,8 +6,8 @@ public class ShoppingCart {
 
     public void displayItems() {
         int sum = 0;
-        System.out.println("장바구니 상품 출력");
         for(int i = 0; i<itemCount; i++){
+        System.out.println("장바구니 상품 출력");
             System.out.println("상품명 : " + items[i].getItem() + ", 합계 : " + items[i].getTotalPrice());
             sum += items[i].getTotalPrice();
         }
