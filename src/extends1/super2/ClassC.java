fe8cca3b1d3fca578482be82186bad8c62d03c10
MOON <moonjs1018@naver.com>
2024-01-23 19:09:38 +0900

    상속 예제 풀이

--- src/extends1/super2/ClassC.java
+++ src/extends1/super2/ClassC.java
@@ -1,2 +1,9 @@
-package extends1.super2;public class ClassC {
+package extends1.super2;
+
+public class ClassC extends ClassB {
+
+    public ClassC() {
+        super(10,10); // 부모의 생성자가 이미 정의되어 있기에 값을 넣어줘야 함.
+        System.out.println("ClassC 생성자");
+    }
 }
